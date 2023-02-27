3ece7cf58f62331cdae5dafef3e9a3d5dc243a24
Vladimir Grekov <grekoff_vv@mail.ru>
2023-02-28 03:00:54 +0700

    homework6 update pom.xml, added annotation AuthController.java

--- lesson6/core-service/src/main/java/com/grekoff/lesson6/core/controllers/AuthController.java
+++ lesson6/core-service/src/main/java/com/grekoff/lesson6/core/controllers/AuthController.java
@@ -13,14 +13,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
+@CrossOrigin("*")
 public class AuthController {
 
     private final UsersService usersService;
