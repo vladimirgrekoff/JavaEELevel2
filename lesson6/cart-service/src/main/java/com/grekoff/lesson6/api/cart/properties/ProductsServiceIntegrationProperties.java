cc58e0aff7b2034d615bc070834331e3b1117767
Vladimir Grekov <grekoff_vv@mail.ru>
2023-03-04 02:16:06 +0700

    homework6 update file ProfileServiceIntegrationProperties

--- lesson6/cart-service/src/main/java/com/grekoff/lesson6/api/cart/properties/ProductsServiceIntegrationProperties.java
+++ lesson6/cart-service/src/main/java/com/grekoff/lesson6/api/cart/properties/ProductsServiceIntegrationProperties.java
@@ -2,9 +2,10 @@ package com.grekoff.lesson6.api.cart.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 //import org.springframework.boot.context.properties.ConstructorBinding;
 
-//@ConstructorBinding
+@Configuration
 @ConfigurationProperties(prefix = "integrations.product-service")
 @Data
 public class ProductsServiceIntegrationProperties {
